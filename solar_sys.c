5847dda4b339585900efdd78097a4acba5743766
palomalacerda <psls@ic.ufal.br>
2022-05-01 12:09:37 -0300

    updating solar system

--- solar_sys.c
+++ solar_sys.c
@@ -6,27 +6,35 @@
 #define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 800
 
-GLfloat angle, fAspect;
+// Angles around the earth
+static GLfloat velocidade;
+static float flag1 = 0.0f;
+static float flag2 = 0.0f;
+static GLfloat angle, fAspect;
 float cam_x, cam_y, cam_z;
 float center_x, center_y, center_z;
 
 //Função responsável pela chamada de gerenciamento do teclado
 void keyboeard_setting(unsigned char key, int x, int y){
     switch (key){
+		case 'X':
         case 'x':
             //Gira ao redor do eixo x
 			glTranslatef (5, 0 , 0);
-            glRotatef(angle, 5,0,0);
+            glRotatef(velocidade, 5,0,0);
+            angle = flag2;
             break;
+		case 'Y':
         case 'y':
             //Gira ao redor do eixo y
 			glTranslatef (0 ,5, 0);
             glRotatef(angle, 0,5, 0);
+            velocidade = flag1;
             break;
-        default: 
-		
-            // printf("ESTIVE AQUI\n");   s
-            break;
+		default:
+			//fecha o sistema
+			exit(0);
+			break;
     }
 
 	// EspecificaParametrosVisualizacao();
@@ -36,15 +44,14 @@ void keyboeard_setting(unsigned char key, int x, int y){
 
 
 // Função usada para especificar o volume de visualização
-void EspecificaParametrosVisualizacao()
-{
+void EspecificaParametrosVisualizacao(){
 	// Especifica sistema de coordenadas de projeção
 	glMatrixMode(GL_PROJECTION);
 	// Inicializa sistema de coordenadas de projeção
 	glLoadIdentity();
 
 	// Especifica a projeção perspectiva
-	gluPerspective(angle,fAspect,0.1,300);
+	gluPerspective(angle,fAspect,0.1,1100);
 
 	// Especifica sistema de coordenadas do modelo
 	glMatrixMode(GL_MODELVIEW);
@@ -52,26 +59,24 @@ void EspecificaParametrosVisualizacao()
 	glLoadIdentity();
 
 	// Especifica posição do observador e do alvo
-	gluLookAt(cam_x,cam_y,cam_z, center_x,center_y,center_z, 0,0,1);
+	gluLookAt(cam_x,cam_y,cam_z, center_x,center_y,center_z, 0,1,1);
 }
 
 // Função callback chamada para gerenciar eventos do mouse
-void GerenciaMouse(int button, int state, int x, int y)
-{
+void GerenciaMouse(int button, int state, int x, int y){
 	if (button == GLUT_LEFT_BUTTON)
 		if (state == GLUT_DOWN) {  // Zoom-in
 			if (angle >= 10) angle -= 5;
 		}
 	if (button == GLUT_RIGHT_BUTTON)
 		if (state == GLUT_DOWN) {  // Zoom-out
-			if (angle <= 130) angle += 5;
+			if (velocidade <= 130) velocidade += 5;
 		}
 	EspecificaParametrosVisualizacao();
 	glutPostRedisplay();
 }
 
-void AlteraTamanhoJanela(GLsizei w, GLsizei h)
-{
+void AlteraTamanhoJanela(GLsizei w, GLsizei h){
 	// Para previnir uma divisão por zero
 	if ( h == 0 ) h = 1;
 
@@ -85,8 +90,6 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 
 // Called to draw scene
 void display(){
-    // Angle of revolution around the nucleus
-    static GLfloat velocidade = 0.0f;
     // Clear the window with current clearing color
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -104,57 +107,59 @@ void display(){
     
     // cor da terra
     glColor3ub(0,0,255);
-    
     // Desenhando a terra
 	glTranslatef(50.0f, 0.0f, -50.0f);
-
     glutSolidSphere(6.0f, 15, 15);
-    
     
     // Save viewing transformation
     glPushMatrix();
     
     // Primeira lua 
-    // Rotate by angle of revolution
+    // Rotate by angle of the moon
     glRotatef(velocidade, 0.0f, 1.0f, 0.0f);
-    
     // Translate out from origin to orbit distance
     glTranslatef(90.0f, 0.0f, 0.0f);
 
     // Restore the viewing transformation
     glPopMatrix();
     
-    // firts moon
+    // firt moon
     glPushMatrix();
-    glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
-    glRotatef(velocidade, 0.0f, 1.0f, 0.0f);
-    glTranslatef(-40.0f, 0.0f, 0.0f);
+    glRotatef(45.0f, 0.0f, 0.0f, 2.0f);
+    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    glTranslatef(-30.0f, 0.0f, 0.0f);
     glColor3ub(192,192,192);
-    glutSolidSphere(6.0f, 15.0f, 15.0f);
+    glutSolidSphere(3.0f, 7.5f, 7.5f);
     glPopMatrix();
 
     // Second Moon
     glPushMatrix();
-    glRotatef(360.0f, -45.0f, 0.0f, 1.0f);
+    glRotatef(360.0f, -45.0f, 0.0f, 2.0f);
     glRotatef(velocidade, 0.0f, 1.0f, 0.0f);
     glTranslatef(0.0f, 0.0f, 30.0f);
     glColor3ub(192,192,192);
-    glutSolidSphere(6.0f, 15, 15);
+    glutSolidSphere(3.0f, 7.5f, 7.5f);
     glPopMatrix();  
-
+    
+    flag1 = velocidade;
+    flag2 = angle;
     // 
+    angle +=10.0f;
     velocidade += 10.0f;
+    if(angle > 360.0f) 
+        angle = 0.0f;
+
     if(velocidade > 360.0f)
         velocidade = 0.0f;
 
     // Show the image
     glutSwapBuffers();
 }
-void starting (void)
-{ 
+void starting (void){ 
     //definindo a cor e o angulo
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     angle= 45;
+    velocidade = 0;
 	cam_z = 0.0f;
 	cam_y = 50.0f;
 	cam_x = 0.0f;
@@ -168,10 +173,10 @@ int main(int argc, char** argv){
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(WINDOW_WIDTH,WINDOW_HEIGHT);
 
-	glutCreateWindow("Meu ponto");
+	glutCreateWindow("Paloma - Solar_System");
     starting();    
     glutKeyboardFunc(keyboeard_setting);
-    glutMouseFunc(GerenciaMouse);
+    // glutMouseFunc(GerenciaMouse);
     glutDisplayFunc(display);
     glutReshapeFunc(AlteraTamanhoJanela);
 
--- treino/point.c
+++ treino/point.c
@@ -5,26 +5,36 @@
 
 #define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 800
-GLfloat angle, fAspect;
+
+// Angles around the earth
+static GLfloat velocidade;
+static float flag1 = 0.0f;
+static float flag2 = 0.0f;
+static GLfloat angle, fAspect;
 float cam_x, cam_y, cam_z;
 float center_x, center_y, center_z;
 
 //Função responsável pela chamada de gerenciamento do teclado
 void keyboeard_setting(unsigned char key, int x, int y){
     switch (key){
+		case 'X':
         case 'x':
             //Gira ao redor do eixo x
-			glTranslatef (0.5, 0 , 0);
-            glRotatef(angle, 1,0,0);
+			glTranslatef (5, 0 , 0);
+            glRotatef(velocidade, 5,0,0);
+            angle = flag2;
             break;
+		case 'Y':
         case 'y':
             //Gira ao redor do eixo y
-			glTranslatef (0 , 0.5, 0);
-            glRotatef(angle, 0,1, 0);
+			glTranslatef (0 ,5, 0);
+            glRotatef(angle, 0,5, 0);
+            velocidade = flag1;
             break;
-        default: 
-            printf("ESTIVE AQUI\n");   
-            break;
+		default:
+			//fecha o sistema
+			exit(0);
+			break;
     }
 
 	// EspecificaParametrosVisualizacao();
@@ -34,15 +44,14 @@ void keyboeard_setting(unsigned char key, int x, int y){
 
 
 // Função usada para especificar o volume de visualização
-void EspecificaParametrosVisualizacao()
-{
+void EspecificaParametrosVisualizacao(){
 	// Especifica sistema de coordenadas de projeção
 	glMatrixMode(GL_PROJECTION);
 	// Inicializa sistema de coordenadas de projeção
 	glLoadIdentity();
 
 	// Especifica a projeção perspectiva
-	gluPerspective(angle,fAspect,0.1,300);
+	gluPerspective(angle,fAspect,0.1,600);
 
 	// Especifica sistema de coordenadas do modelo
 	glMatrixMode(GL_MODELVIEW);
@@ -54,8 +63,7 @@ void EspecificaParametrosVisualizacao()
 }
 
 // Função callback chamada para gerenciar eventos do mouse
-void GerenciaMouse(int button, int state, int x, int y)
-{
+void GerenciaMouse(int button, int state, int x, int y){
 	if (button == GLUT_LEFT_BUTTON)
 		if (state == GLUT_DOWN) {  // Zoom-in
 			if (angle >= 10) angle -= 5;
@@ -68,8 +76,7 @@ void GerenciaMouse(int button, int state, int x, int y)
 	glutPostRedisplay();
 }
 
-void AlteraTamanhoJanela(GLsizei w, GLsizei h)
-{
+void AlteraTamanhoJanela(GLsizei w, GLsizei h){
 	// Para previnir uma divisão por zero
 	if ( h == 0 ) h = 1;
 
@@ -83,8 +90,6 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 
 // Called to draw scene
 void display(){
-    // Angle of revolution around the nucleus
-    static GLfloat fElect1 = 0.0f;
     // Clear the window with current clearing color
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -100,61 +105,63 @@ void display(){
     glColor3ub(255, 255, 0);
     glutSolidSphere(7.0f, 15, 15);
     
-    // blue earth
+    // cor da terra
     glColor3ub(0,0,255);
-    
-    // Draw the electron
-    glTranslatef(50.0f, 0.0f, -50.0f);
-
+    // Desenhando a terra
+	glTranslatef(50.0f, 0.0f, -50.0f);
     glutSolidSphere(6.0f, 15, 15);
-    
-    // First Electron 
     
     // Save viewing transformation
     glPushMatrix();
     
-    // Rotate by angle of revolution
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    
+    // Primeira lua 
+    // Rotate by angle of the moon
+    glRotatef(velocidade, 0.0f, 1.0f, 0.0f);
     // Translate out from origin to orbit distance
     glTranslatef(90.0f, 0.0f, 0.0f);
 
     // Restore the viewing transformation
     glPopMatrix();
     
-    // firts moon
+    // firt moon
     glPushMatrix();
-    glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(-40.0f, 0.0f, 0.0f);
+    glRotatef(45.0f, 0.0f, 0.0f, 2.0f);
+    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    glTranslatef(-30.0f, 0.0f, 0.0f);
     glColor3ub(192,192,192);
-    glutSolidSphere(6.0f, 15.0f, 15.0f);
+    glutSolidSphere(3.0f, 7.5f, 7.5f);
     glPopMatrix();
 
     // Second Moon
     glPushMatrix();
-    glRotatef(360.0f, -45.0f, 0.0f, 1.0f);
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+    glRotatef(360.0f, -45.0f, 0.0f, 2.0f);
+    glRotatef(velocidade, 0.0f, 1.0f, 0.0f);
     glTranslatef(0.0f, 0.0f, 30.0f);
     glColor3ub(192,192,192);
-    glutSolidSphere(6.0f, 15, 15);
+    glutSolidSphere(3.0f, 7.5f, 7.5f);
     glPopMatrix();  
-
+    
+    flag1 = velocidade;
+    flag2 = angle;
     // 
-    fElect1 += 10.0f;
-    if(fElect1 > 360.0f)
-        fElect1 = 0.0f;
+    angle +=10.0f;
+    velocidade += 10.0f;
+    if(angle > 360.0f) 
+        angle = 0.0f;
+
+    if(velocidade > 360.0f)
+        velocidade = 0.0f;
 
     // Show the image
     glutSwapBuffers();
 }
-void starting (void)
-{ 
+void starting (void){ 
     //definindo a cor e o angulo
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     angle= 45;
-	cam_z = 5.0f;
-	cam_y = 80.0f;
+    velocidade = 0;
+	cam_z = 0.0f;
+	cam_y = 50.0f;
 	cam_x = 0.0f;
 	center_x = 0.0f; center_y = 0.0f; center_z = 0.0f;
 
@@ -166,7 +173,7 @@ int main(int argc, char** argv){
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(WINDOW_WIDTH,WINDOW_HEIGHT);
 
-	glutCreateWindow("Meu ponto");
+	glutCreateWindow("Paloma - Solar_System");
     starting();    
     glutKeyboardFunc(keyboeard_setting);
     glutMouseFunc(GerenciaMouse);
