6fe4ee5b443b2e00344c3a4538455a4cc83adec1
palomalacerda <psls@ic.ufal.br>
2022-05-09 22:10:26 -0300

    update solar system

--- solar_sys.c
+++ solar_sys.c
@@ -1,225 +1,167 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <GL/glu.h>
 #include <math.h> 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 800
-#define x_position 10
+#define x_position 1100
 #define y_position 10
 
-// Angles around the earth
-static GLfloat velocidade1, velocidade2, moon1, moon2;
-static float flag1 = 0.0f;
-static float flag2 = 0.0f;
-static float flag3 = 0.0f;
-static float flag4 = 0.0f;
-static GLfloat angle, fAspect;
+// Variaveis para controlar a camera
+float fov_y;
 float cam_x, cam_y, cam_z;
 float center_x, center_y, center_z;
 
-//###############################################################
-//Função responsável pela chamada de gerenciamento do teclado
-void keyboard_setting(unsigned char key, int x, int y){
-    switch (key){
-		case 'X':
-        case 'x':
-            glRotatef(moon1, 3,0, 0);
-            glRotatef(moon2, 0, 3, 0);
-            velocidade1 = flag1;
-            velocidade2 = flag2;
-            break;
-		case 'Y':
-        case 'y':
-			glTranslatef (5, 0 , 0);
-            glRotatef(velocidade1, 5,0,0);
-            velocidade2 = flag2;
-            moon1 = flag3;
-            moon2 = flag4;
-            break;
-		default:
-            // glRotatef(velocidade1, 0,5, 0);
-			// exit(0);
-			break;
+typedef struct coords
+{
+    float x, y, z;
+}coords;
+
+//Angulo de rotação em torno do sol
+static GLfloat fElect1 = 50.0f;
+static GLfloat fElect2 = 360.0f;
+static GLfloat fElect3 = 0.0f;
+static coords example;
+
+void keyboard_planet(unsigned char key, int x, int y){
+    if(key == 'y' || key == 'Y'){
+        example.x = 0.0f; 
+        example.y = 1.0f;
+        example.z = 0.0f;
+          // Increment the angle of revolution
+        fElect1 += 10.0f;
+        if(fElect1 > 360.0f)
+            fElect1 = 0.0f;
+
+        // Increment the angle of revolution
+        fElect2 -= 10.0f;
+        if(fElect2 == 0)
+            fElect2 = 360.0f;
+        // exit(0);
     }
 
-	// EspecificaParametrosVisualizacao();
+    //Atualiza a janela
     glutPostRedisplay();
 }
 
-//###############################################################
-// Função usada para especificar o volume de visualização
-
-void EspecificaParametrosVisualizacao(){
-	// Especifica sistema de coordenadas de projeção
-	glMatrixMode(GL_PROJECTION);
-	// Inicializa sistema de coordenadas de projeção
-	glLoadIdentity();
-
-	// Especifica a projeção perspectiva
-	gluPerspective(angle,fAspect,0.1,1100);
-
-	// Especifica sistema de coordenadas do modelo
-	glMatrixMode(GL_MODELVIEW);
-	// Inicializa sistema de coordenadas do modelo
-	glLoadIdentity();
-
-	// Especifica posição do observador e do alvo
-	gluLookAt(cam_x,cam_y,cam_z, center_x,center_y,center_z, 0,1,1);
+void moon(float x, float y, float z, float distance, float size, float rotationSpeed){
+	//"Translation"
+	glRotatef(rotationSpeed, x, y, z);
+	
+	glTranslatef(0.0f, 0.0f, distance);
+	//Local rotation
+	glRotatef(0.0f, 0.0f, 1.0f, 0.0f);
+	
+	glutSolidSphere(size, 200, 200);
 }
 
-//###############################################################
-// Função callback chamada para gerenciar eventos do mouse
+void planet2(){
+    // glPushMatri/x();
+    // Second planet
+    glColor3ub(255,0,0);
+    
+    //Fazendo a rotação com o angulo setado no eixo y
+    glRotatef(fElect2, example.x, example.y, example.z);
 
-void GerenciaMouse(int button, int state, int x, int y){
-	if (button == GLUT_LEFT_BUTTON)
-		if (state == GLUT_DOWN) {  // Zoom-in
-			if (angle >= 10) angle -= 5;
-		}
-	if (button == GLUT_RIGHT_BUTTON)
-		if (state == GLUT_DOWN) {  // Zoom-out
-			if (angle <= 130) angle += 5;
-		}
-	EspecificaParametrosVisualizacao();
-	glutPostRedisplay();
+    //translada da origem até -90x
+    glTranslatef(-90.0f, 0.0f, 0.0f);
+
+    glutSolidSphere(3.0f, 15.5f, 15.5f);
+
+    //Drawing moons
+    glPushMatrix();
+    glColor3ub(192,192,192);
+    moon(0.0f, 1.0f, 0.0f, 20.0f, 1.0f, fElect2);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor3ub(192,192,192);
+    moon(1.0f, 0.0f, 0.0f, 10.0f, 1.0f, fElect2);
+    glPopMatrix();
+
 }
+void planet1(){
 
-//###############################################################
-//Função responsável pelos ajustes de tela
-void AlteraTamanhoJanela(GLsizei w, GLsizei h){
-	// Para previnir uma divisão por zero
-	if ( h == 0 ) h = 1;
+    //Salvando o estado 
+    glPushMatrix();
 
-	// Especifica o tamanho da viewport
-	glViewport(0, 0, w, h);
+    glColor3ub(0,0,255);
 
-	// Calcula a correção de aspecto
-	fAspect = (GLfloat)w/(GLfloat)h;
-	EspecificaParametrosVisualizacao();
+    //Fazendo a rotação com o angulo setado no eixo y
+    glRotatef(fElect1, example.x, example.y, example.z);
+
+    //translada da origem até +90x
+    glTranslatef(90.0f, 0.0f, 0.0f);
+    glutSolidSphere(3.0f, 15.5f, 15.5f);
+    //restaura o estado
+    glPopMatrix();
+
 }
+void sun(){
 
-//###############################################################
-// Called to draw scene
-void display(){
-    // Clear the window with current clearing color
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // Reset the modelview matrix
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    // Translate the whole scene out and into view
-    // This is the initial viewing transformation
-    glTranslatef(0.0f, 0.0f, -120.0f);
+    // Setando Translatação inicial
+    glTranslatef(0.0f, 0.0f, -300.0f);
     // glRotatef(0, 0,40, 360);
 
     // Yellow sun
     glColor3ub(255, 255, 0);
-    // glScaled(2,2,2);s
     glutSolidSphere(7.0f, 30, 30);
-    // // Save viewing transformation
-    glPushMatrix();
-    // // Primeira lua 
-    // // Rotate by angle of the moon
-    // glRotatef(velocidade2, 0.0f, 1.0f, 0.0f);
-    // // Translate out from origin to orbit distance
-    // glTranslatef(90.0f, 0.0f, 0.0f);
+    planet1();
+    planet2();
 
-    // Restore the viewing transformation
-    // glPopMatrix();
-    
-    // firt planet
-    glPushMatrix();
-    glRotatef(180.0f, 0.0f, 0.0f, 50.0f);
-    glRotatef(velocidade1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(-30.0f, 0.0f, 0.0f);
-    glColor3ub(0,0,255);
-    glutSolidSphere(3.0f, 15.5f, 15.5f);
-    glPopMatrix();
-
-   //##############################################################//
-    // glPushMatri/x();
-    // Second planet
-    glRotatef(360.0f, -45.0f, 0.0f, 2.0f);
-    glRotatef(velocidade1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(10.0f, 0.0f, 30.0f);
-    glColor3ub(255,0,0);
-    glutSolidSphere(3.0f, 15.5f, 15.5f);
-    
-    //Drawing moons
-    glPushMatrix();
-    glColor3ub(192,192,192);
-    glRotatef(moon1, 5.0f, 0.0f, 0.0f);
-    glTranslatef(5.0f, 1.0f, 10.0f);
-    glutSolidSphere(1.0f, 15, 15);
-    glColor3ub(235,192,192);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glRotatef(moon2, 1.0f, 0.0f, 0.0f);
-    glTranslatef(5.0f, 5.0f, 5.0f);
-    // glRotatef(45.0f, 0,0,0);
-    glutSolidSphere(1.0f,15,15);
-    glPopMatrix();
-
-    //##############################################################//
-    flag1 = velocidade1;
-    flag2 = velocidade2;
-    flag3 = moon1;
-    flag4 = moon2;
-    // 
-    angle +=10.0f;
-    velocidade1 -= 10.0f;
-    velocidade2 += 40.0f;
-    moon1 += 4.0f;
-    moon2 += 8.0f;
-    
-    if(angle > 360.0f) 
-        angle = 0.0f;
-
-    if(velocidade1 <=0)
-        velocidade1 = 360.0f;
-
-    if(velocidade2 > 360.0f)
-        velocidade2 = 0.0f;
-
-    if(moon1 > 360.0f)
-        moon1 = 0.0f;
-
-    if(moon2 > 360.0f)
-        moon2 = 0.0f;
-    // Show the image
-    glutSwapBuffers();
 }
 
 //###############################################################
-void starting (){ 
-    //definindo a cor e o angulo
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    angle= 45;
-    velocidade1 = 360;
-    velocidade2 = 0;
-    moon1 = 0;
-    moon2 = 0;
-	cam_z = 0.0f;
-	cam_y = 50.0f;
-	cam_x = 0.0f;
-	center_x = 0.0f; center_y = 10.0f; center_z = 0.0f;
+// Inicio
+void display(){
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    // Resetando a matriz de transformações
+    glLoadIdentity();
+
+    gluLookAt(cam_x, cam_y, cam_z, center_x, center_y, center_z, 0.0f, 1.0f, 0.0f);
+    
+    sun();
+
+    glutSwapBuffers();
 }
+
+
 //###############################################################
 int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(WINDOW_WIDTH,WINDOW_HEIGHT);
-
 	glutCreateWindow("Paloma - Solar_System");
     glutPositionWindow(x_position,y_position);
-    starting();    
-    glutKeyboardFunc(keyboard_setting);
-    // glutMouseFunc(GerenciaMouse);
-    glutDisplayFunc(display);
-    glutReshapeFunc(AlteraTamanhoJanela);
 
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard_planet);
+
+    // Angulo de observação 
+    fov_y = 25.0f;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // angulo, tam de corte, near, far (respectivamente)
+    gluPerspective(fov_y, 1.0f*WINDOW_WIDTH/WINDOW_HEIGHT, 0.1f, 1050.0f);
+    glEnable(GL_DEPTH_TEST);
+
+    cam_z = 5.0f;
+    cam_y = 0.0f;
     glutMainLoop();
     return 0;
 }
+
+
+
+
+// Falar com o nti pra vê a possibilidade
+// Falar com o LCCV 
+// Laccan, Edge, lccv. 
+// Pensar em um artigo 
--- treino/point.c
+++ treino/point.c
@@ -1,251 +1,370 @@
-#include <GL/gl.h>
 #include <GL/glut.h>
-#include <math.h> 
+#include <GL/glu.h>
 #include <stdio.h>
+#include <math.h>
 
-#define WINDOW_WIDTH 1000
-#define WINDOW_HEIGHT 800
+#define min(a, b) ((a) < (b) ? a : b)
+#define max(a, b) ((a) > (b) ? a : b)
+#define radians(a) (a * M_PI / 180.0f)
 
-// // Angles around the earth
-// static GLfloat velocidade;
-// static float flag1 = 0.0f;
-// static float flag2 = 0.0f;
-// static GLfloat angle, fAspect;
-// float cam_x, cam_y, cam_z;
-// float center_x, center_y, center_z;
+enum {
+	X,
+	Y,
+	Z
+};
 
-// //Função responsável pela chamada de gerenciamento do teclado
-// void keyboeard_setting(unsigned char key, int x, int y){
-//     switch (key){
-// 		case 'X':
-//         case 'x':
-//             //Gira ao redor do eixo x
-// 			glTranslatef (5, 0 , 0);
-//             glRotatef(velocidade, 5,0,0);
-//             angle = flag2;
-//             break;
-// 		case 'Y':
-//         case 'y':
-//             //Gira ao redor do eixo y
-// 			glTranslatef (0 ,5, 0);
-//             glRotatef(angle, 0,5, 0);
-//             velocidade = flag1;
-//             break;
-// 		default:
-// 			//fecha o sistema
-// 			exit(0);
-// 			break;
-//     }
+enum {
+	Orthographic,
+	Perspective
+};
 
-// 	// EspecificaParametrosVisualizacao();
-//     glutPostRedisplay();
-// }
+int WINDOW_WIDTH = 640;
+int WINDOW_HEIGHT = 480;
+int MOUSE_X;
+int MOUSE_Y;
+char WINDOW_NAME[] = "OpenGL Application";
+float dt = 0.0f;
+float t = 0.0f;
+float ref_t = 0.0f;
+int projectionMode = Perspective;
+float cam_position[3] = {0.0f, 1.73f, 5.0f};
+float cam_rotation[3] = {0.0f, 0.0f, 0.0f};
+float fovy = 60.0f;
+int walk[3];
+int rotate[3];
+float move_speed = 10.0f;
+float rotation_speed = 180.0f;
+float sensibility[2] = {10.0f, 10.0f};
 
+/* GLUT callbacks */
+void init(int* argc, char** argv, int window_width, int window_height, int display_mode, const char* window_name);
+void idle(void);
+void reshape(int w, int h);
+void motion(int w, int h);
+void keyboard(unsigned char key, int x, int y);
+void keyboard_up(unsigned char key, int x, int y);
+void display();
 
+void draw_line(float x0, float y0, float z0, float x1, float y1, float z1);
+void draw_rectangle(float x, float y, float w, float h, float angle);
+void draw_circle(float x, float y, float radius);
+void draw_floor(float grid_size, int n);
+void draw_axis();
 
-// // Função usada para especificar o volume de visualização
-// void EspecificaParametrosVisualizacao(){
-// 	// Especifica sistema de coordenadas de projeção
-// 	glMatrixMode(GL_PROJECTION);
-// 	// Inicializa sistema de coordenadas de projeção
-// 	glLoadIdentity();
-
-// 	// Especifica a projeção perspectiva
-// 	gluPerspective(angle,fAspect,0.1,600);
-
-// 	// Especifica sistema de coordenadas do modelo
-// 	glMatrixMode(GL_MODELVIEW);
-// 	// Inicializa sistema de coordenadas do modelo
-// 	glLoadIdentity();
-
-// 	// Especifica posição do observador e do alvo
-// 	gluLookAt(cam_x,cam_y,cam_z, center_x,center_y,center_z, 0,0,1);
-// }
-
-// // Função callback chamada para gerenciar eventos do mouse
-// void GerenciaMouse(int button, int state, int x, int y){
-// 	if (button == GLUT_LEFT_BUTTON)
-// 		if (state == GLUT_DOWN) {  // Zoom-in
-// 			if (angle >= 10) angle -= 5;
-// 		}
-// 	if (button == GLUT_RIGHT_BUTTON)
-// 		if (state == GLUT_DOWN) {  // Zoom-out
-// 			if (angle <= 130) angle += 5;
-// 		}
-// 	EspecificaParametrosVisualizacao();
-// 	glutPostRedisplay();
-// }
-
-// void AlteraTamanhoJanela(GLsizei w, GLsizei h){
-// 	// Para previnir uma divisão por zero
-// 	if ( h == 0 ) h = 1;
-
-// 	// Especifica o tamanho da viewport
-// 	glViewport(0, 0, w, h);
-
-// 	// Calcula a correção de aspecto
-// 	fAspect = (GLfloat)w/(GLfloat)h;
-// 	EspecificaParametrosVisualizacao();
-// }
-
-// // Called to draw scene
-// void display(){
-//     // Clear the window with current clearing color
-//     glClear(GL_COLOR_BUFFER_BIT);
-
-//     // Reset the modelview matrix
-//     glMatrixMode(GL_MODELVIEW);
-//     glLoadIdentity();
-
-//     // Translate the whole scene out and into view
-//     // This is the initial viewing transformation
-//     glTranslatef(0.0f, 0.0f, -100.0f);
-
-//     // Yellow sun
-//     glColor3ub(255, 255, 0);
-//     glutSolidSphere(7.0f, 15, 15);
-    
-//     // cor da terra
-//     glColor3ub(0,0,255);
-//     // Desenhando a terra
-// 	glTranslatef(50.0f, 0.0f, -50.0f);
-//     glutSolidSphere(6.0f, 15, 15);
-    
-//     // Save viewing transformation
-//     glPushMatrix();
-    
-//     // Primeira lua 
-//     // Rotate by angle of the moon
-//     glRotatef(velocidade, 0.0f, 1.0f, 0.0f);
-//     // Translate out from origin to orbit distance
-//     glTranslatef(90.0f, 0.0f, 0.0f);
-
-//     // Restore the viewing transformation
-//     glPopMatrix();
-    
-//     // firt moon
-//     glPushMatrix();
-//     glRotatef(45.0f, 0.0f, 0.0f, 2.0f);
-//     glRotatef(angle, 0.0f, 1.0f, 0.0f);
-//     glTranslatef(-30.0f, 0.0f, 0.0f);
-//     glColor3ub(192,192,192);
-//     glutSolidSphere(3.0f, 7.5f, 7.5f);
-//     glPopMatrix();
-
-//     // Second Moon
-//     glPushMatrix();
-//     glRotatef(360.0f, -45.0f, 0.0f, 2.0f);
-//     glRotatef(velocidade, 0.0f, 1.0f, 0.0f);
-//     glTranslatef(0.0f, 0.0f, 30.0f);
-//     glColor3ub(192,192,192);
-//     glutSolidSphere(3.0f, 7.5f, 7.5f);
-//     glPopMatrix();  
-    
-//     flag1 = velocidade;
-//     flag2 = angle;
-//     // 
-//     angle +=10.0f;
-//     velocidade += 10.0f;
-//     if(angle > 360.0f) 
-//         angle = 0.0f;
-
-//     if(velocidade > 360.0f)
-//         velocidade = 0.0f;
-
-//     // Show the image
-//     glutSwapBuffers();
-// }
-// void starting (void){ 
-//     //definindo a cor e o angulo
-//     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-//     angle= 45;
-//     velocidade = 0;
-// 	cam_z = 0.0f;
-// 	cam_y = 50.0f;
-// 	cam_x = 0.0f;
-// 	center_x = 0.0f; center_y = 0.0f; center_z = 0.0f;
-
-// }
-
-// Change viewing volume and viewport.
-void ChangeSize(GLsizei w, GLsizei h)
-{
-    GLfloat fAspect;
-    // Called when window is resized
-    // Prevent a divide by zero
-    if(h == 0)
-        h = 1;
-    // Set viewport to window dimensions
-    glViewport(0, 0, w, h);
-    // Calculate aspect ratio of the window
-    fAspect = (GLfloat)w/(GLfloat)h;
-    // Set the perspective coordinate system
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    // Field of view of 45 degrees, near and far planes 1.0 and 425
-    gluPerspective(45.0f, fAspect, 1.0, 425.0);
-    // Modelview matrix reset
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+void planet(float x, float y, float z, float distance, float size, float rotationSpeed){
+	//"Translation"
+	glRotatef(rotationSpeed * t, x, y, z);
+	
+	glTranslatef(0.0f, 0.0f, distance);
+	//Local rotation
+	glRotatef(0.0f, 0.0f, 1.0f, 0.0f);
+	
+	glutSolidSphere(size, 200, 200);
 }
 
-// Called to draw scene
-    void RenderScene(void){
-    // Earth and moon angle of revolution
-    static float fMoonRot = 0.0f;
-    static float fEarthRot = 0.0f;
-    // Clear the window with current clearing color
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // Save the matrix state and do the rotations
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    // Translate the whole scene out and into view
-    glTranslatef(0.0f, 0.0f, -300.0f);
-    // Set material color, to yellow
-    // Sun
-    glColor3ub(255, 255, 0);
 
-    glDisable(GL_LIGHTING);
-    glutSolidSphere(15.0f, 15, 15);
-    glEnable(GL_LIGHTING);
-    // Position the light after we draw the Sun!
-    glLightfv(GL_LIGHT0,GL_POSITION,lightPos);
-    // Rotate coordinate system
-    glRotatef(fEarthRot, 0.0f, 1.0f, 0.0f);
-    // Draw the earth
-    glColor3ub(0,0,255);
-    glTranslatef(105.0f,0.0f,0.0f);
-    glutSolidSphere(15.0f, 15, 15);
-    // Rotate from Earth-based coordinates and draw moon
-    glColor3ub(200,200,200);
-    glRotatef(fMoonRot,0.0f, 1.0f, 0.0f);
-    glTranslatef(30.0f, 0.0f, 0.0f);
-    fMoonRot+= 15.0f;
-    if(fMoonRot > 360.0f)
-        fMoonRot = 0.0f;
-    glutSolidSphere(6.0f, 15, 15);
-    // Restore the matrix state
-    glPopMatrix();
-    // Modelview matrix
-    // Step Earth orbit 5 degrees
-    fEarthRot += 5.0f;
-    if(fEarthRot > 360.0f)
-        fEarthRot = 0.0f;
-    // Show the image
-    glutSwapBuffers();
+void planet1(){
+	glColor3f(1.0f, 0.0f, 0.0f);
+	planet(0.0f, 1.0f, 0.0f, 100.0f, 15.0f, 1.0f);
 }
+
+void planet2(){
+	glColor3f(0.0f, 0.0f, 1.0f);
+	planet(0.0f, 1.0f, 0.0f, 200.0f, 10.0f, 1.0f);
+
+	glPushMatrix();
+	glColor3f(0.5f, 0.5f, 0.5f);
+	planet(0.0f, 1.0f, 0.0f, 20.0f, 2.0f, 1.0f);
+	glPopMatrix();
+	
+	glPushMatrix();
+	glColor3f(0.25f, 0.25f, 0.25f);
+	planet(1.0f, 0.0f, 0.0f, 50.0f, 3.0f, -1.0f);
+	glPopMatrix();
+}
+
+void sun() {
+	glColor3f(1.0f, 1.0f, 0.0f);
+	planet(0.0f, 1.0f, 0.0f, 0.0f, 30.0f, 1.0f);
+
+	//RedPlanet
+	glPushMatrix();
+	planet1();
+	glPopMatrix();
+
+	//BluPlanet
+	glPushMatrix();
+	planet2();
+	glPopMatrix();
+}
+
+void solarSystem(){
+	sun();
+}
+
+void house(){
+	GLfloat colors[][3] = {
+		{1.0, 1.0, 1.0},
+		{1.0, 0.0, 0.0},
+		{0.0, 1.0, 0.0},
+		{0.0, 0.0, 1.0},
+		{1.0, 1.0, 0.0},
+		{1.0, 0.0, 1.0},
+		{0.0, 1.0, 1.0},
+		{0.5, 0.1, 0.1},
+		{0.5, 0.1, 0.1},
+		{0.5, 0.1, 0.1},
+		{0.5, 0.1, 0.1},
+		{0.5, 0.1, 0.1},
+		{0.5, 0.1, 0.1},
+		{0.5, 0.1, 0.1},
+		{0.4, 0.2, 0.1},
+		{0.4, 0.1, 0.2},
+		{0.5, 0.1, 0.1}
+	};
+	GLfloat vertices[][3] = {
+		{0.0f, 0.0f, 3.0f},
+		{3.0f, 0.0f, 3.0f},
+		{3.0f, 2.0f, 3.0f},
+		{2.0f, 3.0f, 3.0f},
+		{0.0f, 3.0f, 3.0f},
+		{0.0f, 3.0f, 0.0f},
+		{3.0f, 3.0f, 0.0f},
+		{3.0f, 0.0f, 0.0f},
+		{0.0f, 0.0f, 0.0f},
+		{3.0f, 3.0f, 2.0f},
+		{1.5f, 5.0f, 1.5f}
+	};
+	int faces[][5] = {
+		{0, 1, 2, 3, 4},
+		{1, 7, 6, 9, 2},
+		{7, 8, 5, 6},
+		{8, 0, 4, 5},
+		{0, 1, 7, 8},
+		{2, 9, 3},
+		{4, 3, 9, 6, 5},
+		{10, 5, 4},
+		{10, 4, 3},
+		{10, 3, 9},
+		{10, 9, 6},
+		{10, 6, 5}
+	};
+	int count[] = {5, 5, 4, 4, 4, 3, 5, 3, 3, 3, 3, 3};
+	int n_faces = sizeof(faces)/ (5 * sizeof(int));
+
+	int i, j;
+	for(i = 0;i < n_faces;i++){
+		glColor3f(colors[i][X], colors[i][Y], colors[i][Z]);
+		glBegin(GL_POLYGON);
+		for(j = 0;j < count[i];j++){
+			glVertex3f(vertices[faces[i][j]][X], vertices[faces[i][j]][Y], vertices[faces[i][j]][Z]);
+		}
+		glEnd();
+	}
+}
+
+void init(int* argc, char** argv, int window_width, int window_height, int display_mode, const char* window_name){
+	glutInit(argc, argv);
+	glutInitDisplayMode(display_mode);
+	glutInitWindowSize(window_width, window_height);
+	glutCreateWindow(window_name);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_COLOR_MATERIAL);
+	glCullFace(GL_BACK);
+	glPolygonMode(GL_FRONT, GL_FILL);
+
+	reshape(WINDOW_WIDTH, WINDOW_HEIGHT);	
+}
+
+void idle(void) {
+    static float old_t = 0.0f;
+
+    t = glutGet(GLUT_ELAPSED_TIME) / 1000.0 - ref_t;
+    dt = (t - old_t);
+    old_t = t;
+
+    
+    cam_position[Z] -= move_speed * (walk[Z] * sin(radians(cam_rotation[Y]) + M_PI_2) + walk[X] * sin(radians(cam_rotation[Y]))) * dt;
+    cam_position[X] += move_speed * (walk[Z] * cos(radians(cam_rotation[Y]) + M_PI_2) + walk[X] * cos(radians(cam_rotation[Y]))) * dt;
+
+	
+    //gluLookAt(cam_position[X], cam_position[Y], cam_position[Z], cam_rotation[X], cam_rotation[Y], cam_rotation[Z], 0.0f, 1.0f, 0.0f);
+
+    glutPostRedisplay();
+}
+
+void reshape(int w, int h){
+	printf("reshaping\n");
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	
+	glLoadIdentity();
+
+	float aspect = 1.0f * h / w;
+
+	switch(projectionMode){
+		case Orthographic:
+			glOrtho(-WINDOW_WIDTH / 2.0f, WINDOW_WIDTH / 2.0f, (WINDOW_WIDTH * aspect) / 2.0f, (-WINDOW_WIDTH * aspect) / 2.0f, -1000.0f, 1000.0f);
+			break;
+		case Perspective:
+			gluPerspective(fovy, 1.0/aspect, 0.001, 1000.0f);
+	}
+}
+
+void motion(int x, int y){
+	//cam_rotation[X] += dt;
+	static int last[2];
+	cam_rotation[Y] -= (x - last[X]) * sensibility[X] * dt;
+	cam_rotation[X] -= (y - last[Y]) * sensibility[Y] * dt;
+
+	last[X] = x;
+	last[Y] = y;
+}
+
+void keyboard(unsigned char key, int x, int y){
+	if(key == 'r'){
+		ref_t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+	}
+	else if(key == 'c'){
+	    	projectionMode = !projectionMode;
+	    	reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+	}
+	else if (key == 'w'){
+        walk[Z] = 1;	
+	}
+	else if (key == 's'){
+		walk[Z] = -1;	
+	}
+	else if (key == 'd'){
+        walk[X] = 1;	
+	}
+	else if (key == 'a'){
+		walk[X] = -1;	
+	}
+}
+
+void keyboard_up(unsigned char key, int x, int y){
+	if(key == 'w'){
+	    walk[Z] = 0;
+	}
+	else if(key == 's'){
+	    walk[Z] = 0;
+	}
+	else if(key == 'd'){
+	    walk[X] = 0;
+	}
+	else if(key == 'a'){
+	    walk[X] = 0;
+	}
+}
+
+void display(){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	glRotatef(-cam_rotation[X], 1.0, 0.0, 0.0);
+	glRotatef(-cam_rotation[Y], 0.0, 1.0, 0.0);
+
+	glTranslatef(-cam_position[X], -cam_position[Y], -cam_position[Z]);
+
+	draw_floor(1.0f, 50);
+	draw_axis();
+	
+	sun();
+
+ 	glutSwapBuffers();
+}
+
+void draw_circle(float x, float y, float radius){
+	glPushMatrix();
+	glLoadIdentity();
+	glTranslatef(x, y, 0.0f);
+
+	glBegin(GL_POLYGON);
+	float i;
+	for(i = 0.0f; i < 2 * M_PI; i += M_PI / 20){
+		glVertex2f(cos(i) * radius, sin(i) * radius);
+	}
+	glEnd();
+
+	glPopMatrix();
+}
+
+void draw_line(float x0, float y0, float z0, float x1, float y1, float z1){
+	glBegin(GL_LINES);
+		glVertex3f(x0, y0, z0);
+		glVertex3f(x1, y1, z1);
+	glEnd();
+}
+
+void draw_rectangle(float x, float y, float w, float h, float angle){
+	glPushMatrix();
+
+	glMatrixMode(GL_MODELVIEW);
+	
+	glLoadIdentity();
+	glTranslatef(x, y, 0.0f);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+
+	float vertices[][2] = {
+		{-w/2, -h/2},
+		{w/2, -h/2},
+		{w/2, h/2},
+		{-w/2, h/2}
+	};
+
+	glBegin(GL_QUADS);
+	int i;
+	for(i = 0;i < 4;i++){
+		glVertex3f(vertices[i][X], vertices[i][Y], 0.0f);
+	}
+	glEnd();
+
+	glPopMatrix();
+}
+
+void draw_floor(float grid_size, int n){
+	float width = 0.75f;
+	glColor3f(width, width, width);
+	glLineWidth(width);
+	int i, j; 
+	for(i = 1;i < n;i++){
+		draw_line(i * grid_size, 0.0f, n * grid_size, i * grid_size, 0.0f, -n * grid_size);
+		draw_line(-i * grid_size, 0.0f, n * grid_size, -i * grid_size, 0.0f, -n * grid_size);
+		draw_line(n * grid_size, 0.0f, i * grid_size, -n * grid_size, 0.0f, i * grid_size);
+		draw_line(n * grid_size, 0.0f, -i * grid_size, -n * grid_size, 0.0f, -i * grid_size);
+	}
+}
+
+void draw_axis(){
+	float width = 1.5f;
+	glLineWidth(width);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	draw_line(0.0f, 0.0f, 0.0f, 100, 0.0f, 0.0f);	
+	glColor3f(0.0f, 1.0f, 0.0f);
+	draw_line(0.0f, 0.0f, 0.0f, 0.0f, 100, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	draw_line(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 100);
+}
+
 int main(int argc, char** argv){
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+	init(&argc, argv, WINDOW_WIDTH, WINDOW_HEIGHT, GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH, WINDOW_NAME);
 
-	glutCreateWindow("Paloma - Solar_System");
-    // starting();    
-    // glutKeyboardFunc(keyboeard_setting);
-    // glutMouseFunc(GerenciaMouse);
-    glutDisplayFunc(RenderScene);
-    glutReshapeFunc(ChangeSize);
+	glutIdleFunc(idle); 
+	glutReshapeFunc(reshape);
+	glutMotionFunc(motion);
+	glutPassiveMotionFunc(motion);
+	glutKeyboardFunc(keyboard);
+	glutKeyboardUpFunc(keyboard_up);
+	glutDisplayFunc(display);
 
-    glutMainLoop();
-    return 0;
+	glutMainLoop();
+
+	return 0;
 }
